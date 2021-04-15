7a3f607d7d8319d608a68d31fb3c9503434c8a56
Vojtěch Prouza <vprouza@vprouza.cz>
2021-04-15 17:36:02 +0200

    Added supplier

--- items/admin.py
+++ items/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.db import models
-from .models import Item, ItemAdmin, Category
+from .models import Item, ItemAdmin, Category, Supplier
 
 
 # Register your models here.
 admin.site.register(Item, ItemAdmin)
 admin.site.register(Category)
+admin.site.register(Supplier)
 #admin.site.register(ItemAdmin)
 
--- items/models.py
+++ items/models.py
@@ -3,10 +3,9 @@ from django.db import models
 import datetime
 from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(_("name of category"), max_length=255)
-    description = models.TextField(_("description"),default="")
+    description = models.TextField(_("description"),default="", blank=True)
 
     class Meta:
         verbose_name = _("category")
@@ -14,15 +13,27 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Supplier(models.Model):
+    name = models.CharField(_("name of supplier"), max_length=255)
+    description = models.TextField(_("description"),default="", blank=True)
+    link = models.URLField(_("url"),default="", blank=True)
+
+    class Meta:
+        verbose_name = _("supplier")
+        verbose_name_plural = _("suppliers")
+    def __str__(self):
+        return self.name
+
 class Item(models.Model):
-    #list_display('name','count')
     UNITS = [
     ('m', _("metres")),
     ('pcs', _("pieces")),
     ('cm', _("centimetres")),
     ]
     name = models.CharField(_("name"),max_length=255)
+    description = models.TextField(_("description"),default="", blank=True)
     count = models.IntegerField(_("count"),default=0)
+    supplier = models.ForeignKey(Supplier, verbose_name=_("supplier"), on_delete=models.SET_NULL, null=True, blank=True)
     unit = models.CharField(_("unit"),choices=UNITS, max_length=4, default="pcs")
     price = models.DecimalField(_("price per unit"),default=0,decimal_places=2,max_digits=10)
     category = models.ManyToManyField(Category, verbose_name=_("categories"))
@@ -36,4 +47,6 @@ class Item(models.Model):
         return self.name
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'count', 'price', 'date_modified')
+    list_display = ('name', 'count', 'supplier', 'price', 'date_modified')
+    search_fields = ['name', 'description']
+    list_filter = ('supplier',)
